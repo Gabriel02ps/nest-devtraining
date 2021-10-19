c6a131d23e20932cec2697adf2f7ba9a250ac7ab
Gabriel02ps <61991172+Gabriel02ps@users.noreply.github.com>
2021-10-19 08:36:55 -0300

    first commit

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
-  findAll() {
-    return 'Listagem de cursos';
+  findAll(@Res() response) {
+    return response.status(200).send('Listagem de cursos');
   }
 
   @Get(':id')
@@ -13,7 +24,18 @@ export class CoursesController {
   }
 
   @Post()
-  create(@Body('name') body) {
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body('name') body) {
+    return `Atualização do Curso#${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Exclusão do Curso #${id}`;
   }
 }
